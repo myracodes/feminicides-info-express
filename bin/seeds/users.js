c958c451dc9ed019b32e79812c75fec9b0bfac87
Mélusine Rey <zelumine@live.fr>
2021-03-26 17:45:41 +0100

    Add users seeds

---
+++ bin/seeds/users.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+require("../../config/dbConnection");
+const User = require("../../models/User");
+
+const users = [
+  {
+    email: "melu@email.com",
+    password: "123456789",
+    firstName: "Mélu",
+    lastName: "Rey",
+    role: "admin"
+  },
+  {
+    email: "anna@email.com",
+    password: "123456789",
+    firstName: "Anna",
+    lastName: "Lefour",
+    role: "admin"
+  },
+  {
+    email: "anais@email.com",
+    password: "123456789",
+    firstName: "Anaïs",
+    lastName: "Lefour",
+    role: "admin"
+  },
+  {
+    email: "myriam@email.com",
+    password: "123456789",
+    firstName: "Myriam",
+    lastName: "Mira",
+    role: "admin"
+  }
+];
+
+User.create(users)
+.then(createdUsers => console.log("Users created! ", createdUsers))
+.catch(error => console.log(error));
