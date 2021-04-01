657db7d923da143e137b801c4d07e8e7521e1ee6
Anna Lefour <anna.lefour@laposte.net>
2021-04-01 23:02:33 +0200

    Middleware correction

--- middlewares/adminRights.js
+++ middlewares/adminRights.js
@@ -1,13 +1,16 @@
 const User = require("../models/User");
 
 module.exports = function adminRights(req, res, next) {
-  User.findById(req.session.currentUser).then((user) => {
-    console.log(user);
-    if (user.role !== "admin") {
-      return res
-        .status(401)
-        .json({ message: "Unauthorized, you're not an admin" });
-    }
-  });
-  next();
+  User.findById(req.session.currentUser)
+    .then((user) => {
+      console.log(user);
+      if (user.role === "admin") {
+        next();
+      } else {
+        return res
+          .status(401)
+          .json({ message: "Unauthorized, you're not an admin" });
+      }
+    })
+    .catch((err) => console.log(err));
 };
